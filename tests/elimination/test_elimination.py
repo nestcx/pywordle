5122f3a073f924d9ecfe088154e55503bcc9c68e
Marcus <marcusvalentino2013@gmail.com>
2022-03-11 22:08:41 +1100

    add testing directory with tests for word elimination

---
+++ tests/elimination/test_elimination.py
@@ -0,0 +1,94 @@
+from pywordle.pywordle import Wordle
+import json
+
+# using wordlist.txt
+
+def test_edges():
+    game = Wordle(word="edges")
+    f = open('tests/elimination/edges.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_spays():
+    game = Wordle(word="spays")
+    f = open('tests/elimination/spays.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_condo():
+    game = Wordle(word="condo")
+    f = open('tests/elimination/condo.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_lying():
+    game = Wordle(word="lying")
+    f = open('tests/elimination/lying.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_crime():
+    game = Wordle(word="crime")
+    f = open('tests/elimination/crime.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_myrrh():
+    game = Wordle(word="myrrh")
+    f = open('tests/elimination/myrrh.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_tenth():
+    game = Wordle(word="tenth")
+    f = open('tests/elimination/tenth.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_aloof():
+    game = Wordle(word="aloof")
+    f = open('tests/elimination/aloof.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
+
+def test_chief():
+    game = Wordle(word="chief")
+    f = open('tests/elimination/chief.json', 'r')
+
+    guesses = json.load(f)
+
+    for g in guesses:
+        game.turn(g)
+        assert game.get_remaining_words == guesses[g]
