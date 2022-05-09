3b7e15ec6693bf3ef132f9495d446b85e201cb74
Marcus <marcusvalentino2013@gmail.com>
2022-05-09 14:41:36 +1000

    add comments to solver template

--- pywordle/examples/solver_template/solver_template.py
+++ pywordle/examples/solver_template/solver_template.py
@@ -1,23 +1,33 @@
-from pywordle import MY_WORD_LIST
-from pywordle.utils.clear_terminal import clear_terminal
-from pywordle.pywordle.pywordle import Wordle
-from pywordle.utils.solvetracker import SolveTracker
-from alive_progress import alive_bar
 import random
+from alive_progress import alive_bar
+from pywordle.utils import clear_terminal
 
+# import Wordle class
+from pywordle.pywordle.pywordle import Wordle
+
+# import SolveTracker class
+from pywordle.utils.solvetracker import SolveTracker
+
+
+
+# our wonderful algorithm
 def determine_best_guess(game):
     return random.choice(game.get_remaining_answers)
 
 
+# determine how many times to loop
 game_count = len(Wordle().valid_answer_list)
+
 tracker = SolveTracker(turn_limit=6)
 
 with alive_bar(game_count) as bar:
     for i in range(0, game_count):
-
-        game = Wordle(answer_index=i, valid_answer_list = MY_WORD_LIST)
+		
+        # the gametype 'index' sets the answer to the nth answer in the answers list
+        game = Wordle(gametype='index', word_index=i)
 
         while game.state == "active":
+            # the solver returns the it's best 'guess' to play.
             guess = determine_best_guess(game)
             game.turn(guess)
 
